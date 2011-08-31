e160d0afb608b395e3924a1fdae4e1f5795b8e24
Michael Mitchell <michaelmitchell@hush.com>
2011-08-30 19:03:50 -0700

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chelseacourtdesigns::Application.config.secret_token = '7b85fb1a9f913ef918055fa218d2f4181fad5140b82156d61a8b515686503662289e599474bfc5889cb93769e8bb1d2949aa69ea0aab6d712ac3429e760f28ee'
