686a887ca7bfc6cde0b079690d6ac6cb4b44a9e8
Michael Mitchell <michaelmitchell@hush.com>
2011-09-15 02:59:31 -0700

    final round of edits

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
   $(".home #page_container").hide();
   $(".home #page_container").fadeIn(2500);
-})
+  if ($('#scroller').length){
+    $('#scroller').jScrollPane({
+  	  showArrows:true,
+  	  arrowScrollOnHover: true,
+  	  verticalGutter: 20,
+  	  arrowButtonSpeed: 3
+  	});
+  }
+});
